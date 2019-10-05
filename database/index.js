a42ebb99be9f38441351e8b3373971f7ddd5eafa
mr.scrpt <mr.scrpt@gmail.com>
2019-10-05 11:38:24 +0300

    lowdb used

--- database/index.js
+++ database/index.js
@@ -1,43 +1,17 @@
-const data = {};
-data.skills = require('./skills');
-data.products = require('./products');
-data.social = require('./social');
+const {dbProducts, dbSkills, dbSocial} = require('./db');
 
-DATABASE.on('skills/get', response => {
-    console.log(data.skills);
-    response.reply(data.skills);
+
+DATABASE.on('skills/get', async response => {
+    const skills = dbSkills.getState();
+    response.reply(skills);
 });
 
-DATABASE.on('products/get', response => {
-    response.reply(data.products);
+DATABASE.on('products/get', async response => {
+
+    const products = dbProducts.getState();
+    response.reply(products);
 });
-DATABASE.on('social/get', response => {
-    response.reply(data.social);
+DATABASE.on('social/get', async response => {
+    const social = dbSocial.getState();
+    response.reply(social);
 });
-
-
-
-
-DATABASE.on('social/get', response => {
-    response.reply(data);
-});
-
-
-
-
-/*
-DATABASE.on('posts/count', response => {
-    response.reply(posts.length);
-})
-
-DATABASE.on('posts/add', response => {
-    const {
-        title,
-        content,
-        created_at
-    } = response.data;
-
-    posts.push({ title, content, created_at });
-
-    response.reply(true);
-})*/
--- engine/index.js
+++ engine/index.js
@@ -1,20 +1,14 @@
 ENGINE.on('index/get', async response => {
     try {
-        const complex = await Promise.all(
+        const [skills, products, social] = await Promise.all([
             DATABASE.emit('skills/get'),
             DATABASE.emit('products/get'),
             DATABASE.emit('social/get')
+          ]
         );
 
-        const skills = complex[0];
-        const products = complex[1];
-        const social = complex[2];
-
-       /* const skills = await DATABASE.emit('skills/get');
-        const products = await DATABASE.emit('products/get');
-        const social = await DATABASE.emit('social/get');*/
-
         const data = {skills, products, social};
+
         response.reply(data)
     }catch (err) {
         response.replyErr(err);
@@ -22,6 +16,16 @@ ENGINE.on('index/get', async response => {
 });
 
 ENGINE.on('login/get', async response => {
+    try {
+        const social = await DATABASE.emit('social/get', {});
+        const data = {social};
+        response.reply(data)
+    }catch (err) {
+        response.replyErr(err);
+    }
+});
+
+ENGINE.on('login/post', async response => {
     try {
         const social = await DATABASE.emit('social/get', {});
         const data = {social};
